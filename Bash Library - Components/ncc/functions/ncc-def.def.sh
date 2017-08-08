1b1632a3825fbc86a5ba0430dd6e26344a42e458
William Taylor <musicmrman99@gmail.com>
2017-08-08 17:03:13 +0100

    for both server and client, make two communication channels instead of one
    One for input and one for output.

--- Bash Library - Components/ncc/functions/ncc-def.def.sh
+++ Bash Library - Components/ncc/functions/ncc-def.def.sh
@@ -1,5 +1,28 @@
 function ncc {
-	XDG_CONFIG_HOME="$HOME/.ncc/.config" xfce4-terminal --disable-server --geometry='40x30' --title='Netcat Chat Client' --icon="$HOME/.local/share/icons/ncc.png" --command="nc $*" & disown
-	sleep 0.5
-	wmctrl -F -r 'Netcat Chat Client' -b add,skip_taskbar,skip_pager
+	case "$1" in
+		'-l')
+			# ncc -l <in-port> <out-port>
+			XDG_CONFIG_HOME="$HOME/.ncc/.config" xfce4-terminal --disable-server --geometry='40x30' --icon="$HOME/.local/share/icons/ncc.png" --title='Netcat Chat Server (input)' --command="nc -l '$2'" & disown
+			XDG_CONFIG_HOME="$HOME/.ncc/.config" xfce4-terminal --disable-server --geometry='40x30' --icon="$HOME/.local/share/icons/ncc.png" --title='Netcat Chat Server (output)' --command="nc -l '$3'" & disown
+
+			sleep 0.5
+			wmctrl -F -r 'Netcat Chat Server (input)' -b add,skip_taskbar,skip_pager
+			wmctrl -F -r 'Netcat Chat Server (output)' -b add,skip_taskbar,skip_pager
+			;;
+
+		'-c')
+			# ncc -c <connect-address> <in-port> <out-port>
+			XDG_CONFIG_HOME="$HOME/.ncc/.config" xfce4-terminal --disable-server --geometry='40x30' --icon="$HOME/.local/share/icons/ncc.png" --title='Netcat Chat Client (input)' --command="nc '$2' '$3'" & disown
+			XDG_CONFIG_HOME="$HOME/.ncc/.config" xfce4-terminal --disable-server --geometry='40x30' --icon="$HOME/.local/share/icons/ncc.png" --title='Netcat Chat Client (output)' --command="nc '$2' '$4'" & disown
+
+			sleep 0.5
+			wmctrl -F -r 'Netcat Chat Server (input)' -b add,skip_taskbar,skip_pager
+			wmctrl -F -r 'Netcat Chat Server (output)' -b add,skip_taskbar,skip_pager
+			;;
+
+		*)
+			printf '%s\n' "Error: command option not recognised: '$1'" "Info: Must be '-l' or '-c'"
+			return 1
+	esac
+	return 0
 }
--- Bash Library - Components/ncc/functions/ncc.def.sh
+++ Bash Library - Components/ncc/functions/ncc.def.sh
@@ -2,5 +2,9 @@
 const-src functions:ncc-def
 
 # Initialize its globals:
-ncc__default_port=4999
-ncc__default_address=192.168.1.77
+ncc__default_server_in_port=4999
+ncc__default_server_out_port=4998
+
+ncc__default_client_address=192.168.1.77
+ncc__default_client_in_port=4998
+ncc__default_client_out_port=4999
